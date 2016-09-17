a84ae3708099736e7c3731d44ab32b7c01d6210b
mdos-san <mdos-san@student.42.fr>
2016-09-17 19:47:30 +0200

    [dev]: adding fastly c d flag

--- srcs/main.c
+++ srcs/main.c
@@ -13,11 +13,19 @@ static void	test(char *s, void *param, char type)
 		ft_printf(s);
 	if (type == 1)
 		ft_printf(s, (char*)param);
+	if (type == 2)
+		ft_printf(s, *(char*)param);
+	if (type == 3)
+		ft_printf(s, *(int*)param);
 	ft_putendl("\n=======PRINTF======");
 	if (type == 0)
 		printf(s);
 	if (type == 1)
 		printf(s, (char*)param);
+	if (type == 2)
+		printf(s, *(char*)param);
+	if (type == 3)
+		printf(s, *(int*)param);
 	fflush(stdout);
 	ft_putendl("\n===================\n\n");
 }
@@ -28,6 +36,9 @@ int		main()
 	test("", NULL, 0);
 	test("Basic output", NULL, 0);
 	test("s test: %s", (void*)"coucou", 1);
-	test("s test: %%", NULL, 0);
+	char a = 'a';
+	test("c test: %c", (void*)&a, 2);
+	int	i	= -42;
+	test("d test: %d", (void*)&i, 3);
 	return (0);
 }
