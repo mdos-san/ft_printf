87468071ced07519a5af49c53faa9b7460c85dc7
mdos-san <mdos-san@student.42.fr>
2016-09-18 13:07:02 +0200

    [dev]: first version of ft_putwchar

--- srcs/ft_putwchar.c
+++ srcs/ft_putwchar.c
@@ -1,6 +1,11 @@
 #include "libftprintf.h"
+#define MASK_1 0 
+#define MASK_2 49280
+#define MASK_3 14712960
+#define MASK_4 4034953344
 
-static	void	print_bit(int c)
+
+static	char *print_bit(int c)
 {
 	int		i;
 	char	buf[33];
@@ -10,10 +15,7 @@ static	void	print_bit(int c)
 	buf[32] = '\0';
 	while (i < 32)
 	{
-		if (c % 2 == 1)
-			buf[31 - i] = '1';
-		else
-			buf[31 - i] = '0';
+		buf[31 - i] = (c % 2 == 1) ? '1' : '0';
 		c = c / 2;
 		++i;
 	}
@@ -26,11 +28,85 @@ static	void	print_bit(int c)
 	ft_putchar(' ');
 	write(1, buf + 24, 8);
 	ft_putchar('\n');
+	return (ft_strdup(buf));
+}
+
+static void	mask_init(char *m, char nbr)
+{
+	int i;
+
+	i = 0;
+	if (nbr == 2)
+	{
+		while (i < 6)
+		{
+			m[31 - i] = '#';
+			++i;
+		}
+		i += 2;
+		while (i < 13)
+		{
+			m[31 - i] = '#';
+			++i;
+		}
+	}
+	ft_putendl(m);
+}
+
+static void	mask_fill(char *m, char *n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < 32)
+	{
+		if (m[31 - i] == '#')
+		{
+			m[31 - i] = n[31 - j];
+			++j;
+		}
+		++i;
+	}
+	ft_putendl(m);
+}
+
+static int bin_to_int(char *bin)
+{
+	int	i;
+	int ret;
+	double pow;
+
+	i = 0;
+	ret = 0;
+	pow = 1;
+	while (i < 32)
+	{
+		if (bin[31 - i] == '1')
+			ret += pow;
+		pow *= 2;
+		++i;
+	}
+	ft_putnbr(ret);
+	return (ret);
 }
 
 void	ft_putwchar(int c)
 {
-	print_bit(c);
+	char	*mask;
+	char	*nbr;
+	int		result;
 
-
+	mask = print_bit(MASK_2);
+	nbr = print_bit(c);
+	mask_init(mask, 2);
+	mask_fill(mask, nbr);
+	result = bin_to_int(mask);
+	free(nbr);
+	nbr = (char*)&result;
+	write(1, nbr + 3, 1);
+	write(1, nbr + 2, 1);
+	write(1, nbr + 1, 1);
+	write(1, nbr + 0, 1);
 }
