e0e9ff5166e5b19de1cee8ee443a22a7e6b45f4e
Mickael DOS SANTOS <mdos-san@e2r4p7.42.fr>
2016-09-19 15:03:07 +0200

    [dev]: precision S

--- srcs/print_ls.c
+++ srcs/print_ls.c
@@ -1,7 +1,37 @@
 #include "libftprintf.h"
 
+static int	*int_arr_dup(int *arr)
+{
+	int	i;
+	int	nbr;
+	int	*ret;
+	
+	i = 0;
+	nbr = 0;
+	while (arr[nbr])
+		++nbr;
+	ret = (int*)malloc(sizeof(int) * (nbr + 1));
+	ret[nbr] = 0;
+	while (arr[i])
+	{
+		ret[i] = arr[i];
+		++i;
+	}
+	return (ret);
+}
+
 void	print_ls(void *s, int precision)
 {
-	ft_putwstr((int*)s);
-	(void)precision;
+	int		tmp;
+	int		*arr;
+
+	arr = int_arr_dup((int*)s);
+	if (precision > 0)
+	{
+		tmp = arr[precision / 4];
+		arr[precision / 4] = 0;
+	}
+	ft_putwstr(arr);
+	if (precision > 0)
+		arr[precision / 4] = tmp;
 }
