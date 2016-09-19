7cc9475ee44682b82d8b9ec39de954cdad437ded
Mickael DOS SANTOS <mdos-san@e2r4p7.42.fr>
2016-09-19 14:46:18 +0200

    [dev]: precision on s

--- srcs/print_s.c
+++ srcs/print_s.c
@@ -2,6 +2,17 @@
 
 void	print_s(void *s, int precision)
 {
-	ft_putstr((char*)s);
-	(void)precision;
+	char	tmp;
+	char	*str;
+
+	str = ft_strdup((char*)s);
+	if (precision > 0)
+	{
+		tmp = str[precision];
+		str[precision] = '\0';
+	}
+	ft_putstr(str);
+	if (precision > 0)
+		str[precision] = tmp;
+	ft_strdel(&str);
 }
