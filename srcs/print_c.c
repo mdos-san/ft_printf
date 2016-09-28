99d8b3eb49357173190476c0feae931682116f4f
mdos-san <mdos-san@student.42.fr>
2016-09-28 21:41:07 +0200

    fix

--- srcs/print_c.c
+++ srcs/print_c.c
@@ -2,8 +2,7 @@
 
 void	print_c(void *c, t_flag flag, int *r)
 {
-	(flag.flag['-'] == 0 && !flag.flag['0']) ? print_width(flag.width - 1, r) : 0;
-	(flag.flag['-'] == 0 && flag.flag['0']) ? print_width_z(flag.width - 1, r) : 0;
+	(flag.flag['-'] == 0) ? print_width(flag.width - 1, r) : 0;
 	ft_putchar(*(char*)c);
 	++*r;
 	(flag.flag['-'] == 1) ? print_width(flag.width - 1, r) : 0;
