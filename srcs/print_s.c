2d1321af1b0240a8160fc00b1f6e9aaf971c7ee9
Mickael DOS SANTOS <mdos-san@e2r3p4.42.fr>
2016-10-12 12:44:54 +0200

    294/396

--- srcs/print_s.c
+++ srcs/print_s.c
@@ -20,10 +20,13 @@ void	print_s(void *s, t_flag flag, int *r)
 			tmp = str[flag.precision];
 			str[flag.precision] = '\0';
 		}
+		if (flag.width > 0)
+		{
 		nb = flag.width - ft_strlen(str);
 		nb = (flag.p_given && !flag.precision) ? flag.width : nb;
 		(flag.flag['-'] == 0 && !flag.flag['0']) ? print_width(nb, r) : 0;
 		(flag.flag['-'] == 0 && flag.flag['0']) ? print_width_z(nb, r) : 0;
+		}
 		if ((flag.p_given && flag.precision) || !flag.p_given)
 		{
 			((char *)s != NULL) ? ft_putstr(str) : ft_putstr("(null)");
