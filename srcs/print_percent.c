2281839430e5fc664c7d7f3dd2f36399f2b96aa4
Mickael DOS SANTOS <mdos-san@e2r4p6.42.fr>
2016-10-13 11:12:54 +0200

    366/374

--- srcs/print_percent.c
+++ srcs/print_percent.c
@@ -2,7 +2,8 @@
 
 void	print_percent(void *arg, t_flag flag, int *ret)
 {
-	(flag.flag['-'] == 0) ? print_width(flag.width - 1, ret) : 0;
+	(!flag.flag['-'] && !flag.flag['0']) ? print_width(flag.width - 1, ret) : 0;
+	(!flag.flag['-'] && flag.flag['0']) ? print_width_z(flag.width - 1, ret) : 0;
 	ft_putchar('%');
 	++*ret;
 	(flag.flag['-'] == 1) ? print_width(flag.width - 1, ret) : 0;
